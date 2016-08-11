37d1c8606c64158559996a6c186c846a2911368d
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2016-08-11 17:45:43 -0600

    Adding yaml-cpp to the source code and makefile

--- sourceMods.sh
+++ sourceMods.sh
@@ -1,2 +1,3 @@
 module purge
 module load gcc/4.8.1 lapack/3.4.2/gcc mkl
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/gvijayak/fastv8DriverProgram/fastv8/CertTest/5MW_Baseline/ServoData/:~/fastv8DriverProgram/fastv8/bin/:~/fastv8DriverProgram/fastv8/CertTest/5MW_Baseline/ServoData/:~/fastv8DriverProgram/fastv8/Source/dependencies/yaml-cpp/lib/
