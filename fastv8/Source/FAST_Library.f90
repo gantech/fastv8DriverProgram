3dfe1f0ce806e6f1953c5abfb1502a301e62eba5
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2016-08-24 12:57:08 -0600

    Changing Turbine array to C-style indexing starting at 0
    Also update nt_global only after FAST_OpFM_Step is called for the last turbine

--- fastv8/Source/FAST_Library.f90
+++ fastv8/Source/FAST_Library.f90
@@ -53,7 +53,7 @@ subroutine FAST_AllocateTurbines(nTurbines, ErrStat_c, ErrMsg_c) BIND (C, NAME='
       call wrscr1('Proceeding anyway')
    end if
 
-   allocate(Turbine(NumTurbines))
+   allocate(Turbine(0:NumTurbines-1)) !Allocate in C style because most of the other Turbine properties from the input file are in C style inside the C++ driver
 
 end subroutine FAST_AllocateTurbines
 
@@ -613,22 +613,26 @@ subroutine FAST_OpFM_Step(iTurb, ErrStat_c, ErrMsg_c) BIND (C, NAME='FAST_OpFM_S
    IF ( n_t_global > Turbine(iTurb)%p_FAST%n_TMax_m1 ) THEN !finish 
       
       ! we can't continue because we might over-step some arrays that are allocated to the size of the simulation
-
-      IF (n_t_global == Turbine(iTurb)%p_FAST%n_TMax_m1 + 1) THEN  ! we call update an extra time in Simulink, which we can ignore until the time shift with outputs is solved
-         n_t_global = n_t_global + 1
-         ErrStat_c = ErrID_None
-         ErrMsg = C_NULL_CHAR
-         ErrMsg_c = TRANSFER( ErrMsg//C_NULL_CHAR, ErrMsg_c )
-      ELSE     
-         ErrStat_c = ErrID_Info
-         ErrMsg = "Simulation completed."//C_NULL_CHAR
-         ErrMsg_c = TRANSFER( ErrMsg//C_NULL_CHAR, ErrMsg_c )
-      END IF
+      
+      if (iTurb .eq. (NumTurbines-1) ) then
+         IF (n_t_global == Turbine(iTurb)%p_FAST%n_TMax_m1 + 1) THEN  ! we call update an extra time in Simulink, which we can ignore until the time shift with outputs is solved
+            n_t_global = n_t_global + 1
+            ErrStat_c = ErrID_None
+            ErrMsg = C_NULL_CHAR
+            ErrMsg_c = TRANSFER( ErrMsg//C_NULL_CHAR, ErrMsg_c )
+         ELSE     
+            ErrStat_c = ErrID_Info
+            ErrMsg = "Simulation completed."//C_NULL_CHAR
+            ErrMsg_c = TRANSFER( ErrMsg//C_NULL_CHAR, ErrMsg_c )
+         END IF
+      end if
       
    ELSE
 
       CALL FAST_Solution_T( t_initial, n_t_global, Turbine(iTurb), ErrStat, ErrMsg )                  
-      n_t_global = n_t_global + 1
+      if (iTurb .eq. (NumTurbines-1) ) then
+         n_t_global = n_t_global + 1
+      end if
             
       ErrStat_c = ErrStat
       ErrMsg = TRIM(ErrMsg)//C_NULL_CHAR
