857f4dcb0656677106977fa9b9b56a05244b05ad
hlong <hai.long@nrel.gov>
2016-09-16 16:15:09 -0600

    Updating compile options for intel compilers

--- sourceMods.sh
+++ sourceMods.sh
@@ -1,5 +1,7 @@
 module purge
-module load gcc/4.8.1 lapack/3.4.2/gcc mkl cmake/3.3.2 openmpi-gcc/1.6.4-4.8.1   boost/1.55.0/openmpi-gcc hdf5/1.8.12/openmpi-gcc
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:FASTDIR/fastv8/bin/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:FASTDIR/fastv8/CertTest/5MW_Baseline/ServoData/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:FASTDIR/fastv8/Source/dependencies/yaml-cpp/lib/
+module use /nopt/nrel/apps/modules/candidate/modulefiles/
+module load impi-intel/5.1.3-16.0.2 lapack/3.4.2/intel-16.0.2 mkl/16.2.181 cmake/3.3.2
+module unload epel
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hlong/ptmp/fast2/intel5/fastv8DriverProgram/fastv8/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hlong/ptmp/fast2/intel5/fastv8DriverProgram/fastv8/CertTest/5MW_Baseline/ServoData/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hlong/ptmp/fast2/intel5/fastv8DriverProgram/fastv8/Source/dependencies/yaml-cpp/lib/
